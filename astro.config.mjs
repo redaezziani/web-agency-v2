03294995db778d3487db092dbfd989cac0d5b1ec
redaezziani <klausdev2@gmail.com>
2024-08-28 14:01:42 +0100

    chore: Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
 
-import node from "@astrojs/node";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  })
+  
 });
